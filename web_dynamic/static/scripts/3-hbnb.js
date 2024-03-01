00668d8646a38a16b1b003c24cca72c0fc36d1fe
Mariem Ehab <mariem.ehab234@gmail.com>
2024-03-01 21:22:43 +0200

    my bad the api do exist. Task 4 finished

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -27,34 +27,36 @@ $(document).ready(function () {
         }
     });
 
-  // const placesURL = "http://0.0.0.0:5001/api/v1/places_search/"
-  const placesURL = "http://localhost:5001/api/v1/cities/05b0b99c-f10e-4e3a-88d1-b3187d6998ee/places";
-  $.get(placesURL, (data) => {
-          const placesSection = $('.places');
+  $.ajax({
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({}),
+    success: (data) => {
+        const placesSection = $('.places');
 
-          data.forEach(place => {
-              const article = $('<article>');
+        data.forEach(place => {
+            const article = $('<article>');
 
-              const titleBox = $('<div>').addClass('title_box');
-              titleBox.append($('<h2>').text(place.name));
-              titleBox.append($('<div>').addClass('price_by_night').text('$' + place.price_by_night));
-              article.append(titleBox);
+            const titleBox = $('<div>').addClass('title_box');
+            titleBox.append($('<h2>').text(place.name));
+            titleBox.append($('<div>').addClass('price_by_night').text('$' + place.price_by_night));
+            article.append(titleBox);
 
-              const information = $('<div>').addClass('information');
-              information.append($('<div>').addClass('max_guest').text(place.max_guest + ' Guest' + (place.max_guest != 1 ? 's' : '')));
-              information.append($('<div>').addClass('number_rooms').text(place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's' : '')));
-              information.append($('<div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's' : '')));
-              article.append(information);
+            const information = $('<div>').addClass('information');
+            information.append($('<div>').addClass('max_guest').text(place.max_guest + ' Guest' + (place.max_guest != 1 ? 's' : '')));
+            information.append($('<div>').addClass('number_rooms').text(place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's' : '')));
+            information.append($('<div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's' : '')));
+            article.append(information);
 
-              if (place.user) {
-                  const user = $('<div>').addClass('user').html('<b>Owner:</b> ' + place.user.first_name + ' ' + place.user.last_name);
-                  article.append(user);
-              }
+            const description = $('<div>').addClass('description').html(place.description);
+            article.append(description);
 
-              const description = $('<div>').addClass('description').html(place.description);
-              article.append(description);
-
-              placesSection.append(article);
-          });
-      });
+            placesSection.append(article);
+        });
+    },
+    error: (xhr, status, error) => {
+        console.error("Error:", error);
+    }
+  });
 });
